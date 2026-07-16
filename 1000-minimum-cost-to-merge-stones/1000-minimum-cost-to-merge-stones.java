785f62d00f08d35d4bbee550fd883ecbec7e9e4e
Aditya Bansal <adityabansalsafe@gmail.com>
2026-07-17 00:54:44 +0530

    Time: 6 ms (7.78%), Space: 44.5 MB (13.23%) - LeetHub

---
+++ 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.java
@@ -0,0 +1,46 @@
+// Top-Down
+class Solution {
+    int[][][] dp;
+    int max = 99999999;
+    int K;
+    
+    public int mergeStones(int[] stones, int K) {
+        this.K = K;
+        int len = stones.length;
+        if ((len - 1) % (K - 1) != 0) {
+            return -1;
+        }
+        dp = new int[len + 1][len + 1][K + 1];
+        int[] prefixSum = new int[len + 1];
+        
+        int i;
+        for (i = 1; i <= len; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i - 1];
+        }
+        
+        return getResult(prefixSum, 1, len, 1);
+    }
+    
+    private int getResult(int[] prefixSum, int left, int right, int piles) {
+        if (dp[left][right][piles] != 0) {
+            return dp[left][right][piles];
+        }
+        int res = max;
+        int t;
+        if (left == right) {
+            res = (piles == 1) ? 0 : max;
+        }
+        else {
+            if (piles == 1) {
+                res = getResult(prefixSum, left, right, K) + prefixSum[right] - prefixSum[left - 1]; 
+            }
+            else {
+                for (t = left; t < right; t++) {
+                    res = Math.min(res, getResult(prefixSum, left, t, piles - 1) + getResult(prefixSum, t + 1, right, 1));
+                }
+            }
+        }
+        dp[left][right][piles] = res;
+        return res;
+    }
+}
